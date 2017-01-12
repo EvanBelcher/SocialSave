2645d4bd4b4a73c11a59fb47a388eff1b3726c4b
Puyush Goel <puyushgoel@gmail.com>
2017-01-12 10:28:49 -0500

    start up activity for the app

---
+++ app/src/main/java/com/yesand/socialsave/join_Group_Actvity.java
@@ -0,0 +1,13 @@
+package com.yesand.socialsave;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class join_Group_Actvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join__group__actvity);
+    }
+}
