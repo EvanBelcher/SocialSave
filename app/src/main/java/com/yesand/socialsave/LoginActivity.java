2efc3be3f9bf87dfe7a8372d0c486b5a158424de
EbMinor3 <evanbelcher3@gmail.com>
2017-01-12 01:43:12 -0500

    Test

--- app/src/main/java/com/yesand/socialsave/LoginActivity.java
+++ app/src/main/java/com/yesand/socialsave/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Just making sure this works
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
