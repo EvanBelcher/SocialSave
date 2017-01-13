d703038bfa850c988c54cdf9d2146d4e96c5ca50
Serina Chang <sc3003@columbia.edu>
2017-01-12 22:29:05 -0500

    Backend done for group creation

--- app/src/main/java/com/yesand/socialsave/CreateAGroupActivity.java
+++ app/src/main/java/com/yesand/socialsave/CreateAGroupActivity.java
@@ -1,23 +1,36 @@
 package com.yesand.socialsave;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateAGroupActivity extends AppCompatActivity {
+
+    private DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_a_group_);
-        final EditText groupId = (EditText)findViewById(R.id.groupName);
+        final EditText group = (EditText)findViewById(R.id.groupName);
         final Button create = (Button)findViewById(R.id.create);
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String groupName = group.getText().toString();
+                DatabaseReference newGroup = dbRef.child("groups").push(); //creates group
+                newGroup.child("name").setValue(groupName);
+                String groupKey = newGroup.getKey(); //gets generated key
+                String confirmation = "Congrats on creating " + groupName + "! Here is your group id: " + groupKey
+                        +". Invite your frineds & they can join your group with this key.";
+                Toast.makeText(CreateAGroupActivity.this, confirmation, Toast.LENGTH_LONG).show();
             }
         });
     }
