02842e82eb0906c902c29d1c9dc26075f10ebd84
Serina Chang <sc3003@columbia.edu>
2017-01-13 00:14:25 -0500

    Saving for now

--- app/src/main/java/com/yesand/socialsave/LogginIntoMain.java
+++ app/src/main/java/com/yesand/socialsave/LogginIntoMain.java
@@ -1,13 +1,34 @@
 package com.yesand.socialsave;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogginIntoMain extends AppCompatActivity {
+
+    private FirebaseAuth fbAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loggin_into_main);
+
+        fbAuth.signInWithEmailAndPassword("serina","123456").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    //go to TeamFragment
+                }
+                else {
+                    Toast.makeText(LogginIntoMain.this, "Login failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
