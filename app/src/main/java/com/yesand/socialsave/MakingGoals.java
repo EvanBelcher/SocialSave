25c0f9f4501f16191b1789f61b3badde75f388c4
Serina Chang <sc3003@columbia.edu>
2017-01-12 19:52:51 -0500

    Editing making goals activity

--- app/src/main/java/com/yesand/socialsave/MakingGoals.java
+++ app/src/main/java/com/yesand/socialsave/MakingGoals.java
@@ -2,6 +2,10 @@ package com.yesand.socialsave;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MakingGoals extends AppCompatActivity {
 
@@ -9,5 +13,41 @@ public class MakingGoals extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_making_goals);
+
+        EditText amount = (EditText)findViewById(R.id.amount);
+        RadioButton isWeekly = (RadioButton)findViewById(R.id.weekIncome);
+        RadioButton isMonthly = (RadioButton)findViewById(R.id.monthIncome);
+        RadioButton isYearly = (RadioButton)findViewById(R.id.yearIncome);
+
+        Button createGroup = (Button)findViewById(R.id.createGroup);
+        createGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateInfo();
+            }
+        });
+
+        Button joinGroup = (Button)findViewById(R.id.joinGroup);
+        joinGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateInfo();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    private void updateInfo()
+    {
+
     }
 }
