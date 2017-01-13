18158375fffcee1d8de161040f7d8087191b6c07
nb5hd <nb5hd@virginia.edu>
2017-01-12 23:24:47 -0500

    Updated XML on making_goals

--- app/src/main/java/com/yesand/socialsave/LoginActivity.java
+++ app/src/main/java/com/yesand/socialsave/LoginActivity.java
@@ -2,9 +2,11 @@ package com.yesand.socialsave;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Window window = LoginActivity.getWindow();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
