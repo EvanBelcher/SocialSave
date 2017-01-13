1a956820b65fd23f187caf3fe6ded3dba059f2d4
Serina Chang <sc3003@columbia.edu>
2017-01-12 21:04:17 -0500

    First draft of making goals backend

--- app/src/main/java/com/yesand/socialsave/MakingGoals.java
+++ app/src/main/java/com/yesand/socialsave/MakingGoals.java
@@ -6,24 +6,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.SeekBar;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MakingGoals extends AppCompatActivity {
+
+    private DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+    private final String INCOME_KEY = "incomePerWeek";
+    private final String GOAL_KEY = "goal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_making_goals);
 
-        EditText amount = (EditText)findViewById(R.id.amount);
-        RadioButton isWeekly = (RadioButton)findViewById(R.id.weekIncome);
-        RadioButton isMonthly = (RadioButton)findViewById(R.id.monthIncome);
-        RadioButton isYearly = (RadioButton)findViewById(R.id.yearIncome);
+        final EditText amount = (EditText)findViewById(R.id.amount);
+        final RadioButton isWeekly = (RadioButton)findViewById(R.id.weekIncome);
+        final RadioButton isMonthly = (RadioButton)findViewById(R.id.monthIncome);
+        final RadioButton isYearly = (RadioButton)findViewById(R.id.yearIncome);
+
+        final SeekBar bar = (SeekBar)findViewById(R.id.seekBar2);
 
         Button createGroup = (Button)findViewById(R.id.createGroup);
         createGroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateInfo();
+                updateInfo(amount, isWeekly, isMonthly, isYearly, bar);
             }
         });
 
@@ -31,7 +41,7 @@ public class MakingGoals extends AppCompatActivity {
         joinGroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateInfo();
+                updateInfo(amount, isWeekly, isMonthly, isYearly, bar);
             }
         });
     }
@@ -46,8 +56,23 @@ public class MakingGoals extends AppCompatActivity {
         super.onStop();
     }
 
-    private void updateInfo()
+    private void updateInfo(EditText amount, RadioButton isWeekly, RadioButton isMonthly, RadioButton isYearly, SeekBar bar)
     {
+        double income = 0.0;
+        double amt = Double.parseDouble(amount.getText().toString());
+        if (isWeekly.isChecked()) {
+            income = amt;
+        }
+        else if (isMonthly.isChecked()) {
+            income = amt * 12 / 52;
+        }
+        else if (isYearly.isChecked()) {
+            income = amt / 52;
+        }
+        SignUpActivity.CURR_USER.child(INCOME_KEY).setValue(income);
 
+        double percentage = bar.getProgress() / 10.0;
+        double goal = income * percentage;
+        SignUpActivity.CURR_USER.child(GOAL_KEY).setValue(goal);
     }
 }
