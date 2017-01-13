ab1d026579144d772cdad40b7452326b0e19b6f2
nb5hd <nb5hd@virginia.edu>
2017-01-13 00:39:21 -0500

    Merge branch 'master' of /Users/nikhil/AndroidStudioProjects/SocialSave with conflicts.

--- app/src/main/java/com/yesand/socialsave/MakingGoals.java
+++ app/src/main/java/com/yesand/socialsave/MakingGoals.java
@@ -3,6 +3,7 @@ package com.yesand.socialsave;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -21,41 +22,41 @@ public class MakingGoals extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_making_goals);
 
-        final EditText amount = (EditText)findViewById(R.id.amount);
+        Window window = getWindow();
+        window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+       // final EditText amount = (EditText)findViewById(R.id.amount);
         final RadioButton isWeekly = (RadioButton)findViewById(R.id.weekIncome);
         final RadioButton isMonthly = (RadioButton)findViewById(R.id.monthIncome);
         final RadioButton isYearly = (RadioButton)findViewById(R.id.yearIncome);
 
         final SeekBar bar = (SeekBar)findViewById(R.id.seekBar2);
 
-        Button createGroup = (Button)findViewById(R.id.createGroup);
-        createGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateInfo(amount, isWeekly, isMonthly, isYearly, bar);
-                // flow - go to CreateAGroupActivity
-            }
-        });
-
-        Button joinGroup = (Button)findViewById(R.id.joinGroup);
-        joinGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateInfo(amount, isWeekly, isMonthly, isYearly, bar);
-                // flow - go to JoinGroupActivity
-            }
-        });
+//        Button createGroup = (Button)findViewById(R.id.createGroup);
+//        createGroup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                updateInfo(amount, isWeekly, isMonthly, isYearly, bar);
+//            }
+//        });
+//
+//        Button joinGroup = (Button)findViewById(R.id.joinGroup);
+//        joinGroup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                updateInfo(amount, isWeekly, isMonthly, isYearly, bar);
+//            }
+//        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//    }
 
     private void updateInfo(EditText amount, RadioButton isWeekly, RadioButton isMonthly, RadioButton isYearly, SeekBar bar)
     {
